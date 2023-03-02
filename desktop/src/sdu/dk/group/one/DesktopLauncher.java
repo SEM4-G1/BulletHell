0967b285697ef84d6b1c44cf2bef9ce6f1164ee2
Magnus Christian Larsen <macl@itconfidence.dk>
2023-03-02 14:23:50 +0100

    Removed ashley from grade file

--- desktop/src/sdu/dk/group/one/DesktopLauncher.java
+++ desktop/src/sdu/dk/group/one/DesktopLauncher.java
@@ -2,7 +2,7 @@ package sdu.dk.group.one;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import sdu.dk.group.one.CoreEngine;
+// import sdu.dk.group.one.CoreEngine;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
