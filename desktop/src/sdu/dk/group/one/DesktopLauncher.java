93e94c7e26dde9cab9234cda02eb9f5db733cb4b
Magnus Christian Larsen <macl@itconfidence.dk>
2023-03-02 13:58:58 +0100

    Fixed spelling mistake

--- desktop/src/sdu/dk/group/one/DesktopLauncher.java
+++ desktop/src/sdu/dk/group/one/DesktopLauncher.java
@@ -2,14 +2,14 @@ package sdu.dk.group.one;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import sdu.dk.group.one.coreEngine;
+import sdu.dk.group.one.CoreEngine;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("bullethell");
-		new Lwjgl3Application(new coreEngine(), config);
+		new Lwjgl3Application(new CoreEngine(), config);
 	}
 }
