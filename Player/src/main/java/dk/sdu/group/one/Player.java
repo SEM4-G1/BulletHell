0e41271590d35c0bbb8caad33883fac1524c69fd
Magnus Christian Larsen <macl@itconfidence.dk>
2023-04-16 17:19:48 +0200

    Added Player module

---
+++ Player/src/main/java/dk/sdu/group/one/Player.java
@@ -0,0 +1,16 @@
+package dk.sdu.group.one;
+
+import dk.sdu.group.one.data.Entity;
+import dk.sdu.group.one.map.MapService;
+
+public class Player extends Entity {
+    @Override
+    public void process() {
+
+    }
+
+    @Override
+    public void start(MapService mapService) {
+
+    }
+}
