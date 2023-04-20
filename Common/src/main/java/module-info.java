e4572197fcd51b02f0e90ace6d2ca4a5d19b97b3
OthelloEngineer <jolee18@student.sdu.dk>
2023-04-20 14:17:43 +0200

    added weapon

--- Common/src/main/java/module-info.java
+++ Common/src/main/java/module-info.java
@@ -2,4 +2,6 @@ module Common {
     exports dk.sdu.group.one.services;
     exports dk.sdu.group.one.data;
     exports dk.sdu.group.one.map;
+    exports dk.sdu.group.one.event.events;
+    exports dk.sdu.group.one.event;
 }
