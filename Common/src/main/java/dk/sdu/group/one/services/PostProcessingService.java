de923fffbce2710da776cace7b4690812a8d16af
OthelloEngineer <jolee18@student.sdu.dk>
2023-04-20 13:50:50 +0200

    fixed git ignore and postprocessing

--- Common/src/main/java/dk/sdu/group/one/services/PostProcessingService.java
+++ Common/src/main/java/dk/sdu/group/one/services/PostProcessingService.java
@@ -1,11 +1,12 @@
 package dk.sdu.group.one.services;
 
-import dk.sdu.group.one.data.EntityList;
+import dk.sdu.group.one.data.EntityManager;
+
 public interface PostProcessingService {
     /**
      *Runs once for each implementation on each game loop
      *
-     * @param entitylist list of entities
+     * @param entityManager list of entities
      */
-    void postProcess(EntityList entitylist);
+    void postProcess(EntityManager entityManager);
 }
