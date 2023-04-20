95969b5c97d11646ebcb3fb525bc2c2b4c40de44
OthelloEngineer <jolee18@student.sdu.dk>
2023-04-20 15:03:30 +0200

    fixed pistol pickup

--- Weapon/src/main/java/dk/sdu/group/weapon/weapontypes/Pistol.java
+++ Weapon/src/main/java/dk/sdu/group/weapon/weapontypes/Pistol.java
@@ -1,5 +1,7 @@
 package dk.sdu.group.weapon.weapontypes;
 
+import dk.sdu.group.one.event.EventBroker;
+import dk.sdu.group.one.event.events.EventType;
 import dk.sdu.group.weapon.Weapon;
 import dk.sdu.group.one.data.EntityManager;
 import dk.sdu.group.one.data.EntityType;
@@ -18,6 +20,7 @@ public class Pistol extends Weapon {
     @Override
     public void start(MapService mapService, EntityManager entityList) {
         Pistol pistol = new Pistol(EntityType.Weapon, "assets/pistol.png", 0, 0);
+        EventBroker.getInstance().subscribe(EventType.PickUpEvent, pistol.pickUpPart);
         entityList.addEntity(pistol);
     }
 }
