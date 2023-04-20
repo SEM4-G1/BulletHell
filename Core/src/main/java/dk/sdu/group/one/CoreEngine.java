090fc18ef8b3b26c3424494190a39663c76ee60e
OthelloEngineer <jolee18@student.sdu.dk>
2023-04-20 14:43:56 +0200

    weapon works

--- Core/src/main/java/dk/sdu/group/one/CoreEngine.java
+++ Core/src/main/java/dk/sdu/group/one/CoreEngine.java
@@ -4,18 +4,22 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import dk.sdu.group.one.data.EntityType;
+import dk.sdu.group.one.data.Entity;
 import dk.sdu.group.one.data.EntityManager;
+import dk.sdu.group.weapon.weapontypes.Pistol;
+
 
 public class CoreEngine extends ApplicationAdapter {
     SpriteBatch batch;
-    Texture img;
     EntityManager entityManager;
 
     @Override
     public void create() {
         this.entityManager = new EntityManager();
         batch = new SpriteBatch();
-        img = new Texture(Gdx.files.internal("assets/test.jpg"));
+        entityManager.addEntity(new Pistol(EntityType.Weapon, "gun.png", 0, 0));
+//        img = new Texture(Gdx.files.internal("assets/test.jpg"));
     }
 
 
@@ -24,7 +28,14 @@ public class CoreEngine extends ApplicationAdapter {
         update();
         Gdx.gl.glClearColor(1, 0, 0, 1);
         batch.begin();
-        batch.draw(img, 0, 0);
+
+        for (Entity entity : entityManager.getEntityList()) {
+            System.out.println(entity.getTexturePath());
+            batch.draw(
+                    new Texture(Gdx.files.internal("assets/" + entity.getTexturePath())),
+                    entity.getX(),
+                    entity.getY());
+        }
         batch.end();
     }
 
@@ -35,7 +46,7 @@ public class CoreEngine extends ApplicationAdapter {
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+//        img.dispose();
     }
     public int multiply(int a, int b){
         return a*b;
