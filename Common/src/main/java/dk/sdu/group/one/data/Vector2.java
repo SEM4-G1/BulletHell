0b6141d9a09224a8dd4a8b231be7c55ef36e77bb
Magnus Christian Larsen <magn3323@gmail.com>
2023-05-04 12:49:56 +0200

    JavaDoc and additional helper functions

--- Common/src/main/java/dk/sdu/group/one/data/Vector2.java
+++ Common/src/main/java/dk/sdu/group/one/data/Vector2.java
@@ -8,20 +8,48 @@ public class Vector2 {
         this.y = y;
     }
 
+    /**
+     * Calculates the magnitude of the Vector2 object.
+     * @return the vector magnitude
+     */
     public float getMagnitude() {
         return (float) Math.sqrt(x*x + y*y);
     }
 
+    /**
+     * Normalizes the Vector2
+     * @return the normalized vector
+     */
     public Vector2 normalize() {
-        return new Vector2(x/getMagnitude(), y/getMagnitude());
+        this.x = x/getMagnitude();
+        this.y = y/getMagnitude();
+        return this;
     }
 
+    /**
+     *
+     * @return x value of vector object.
+     */
     public float getX() {
         return this.x;
     }
 
+    /**
+     *
+     * @return y value of vector object.
+     */
     public float getY() {
         return this.y;
+    }
 
+    /**
+     * Sets the x and y value of the vector
+     * @param x
+     * @param y
+     * @return
+     */
+    public float set(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
