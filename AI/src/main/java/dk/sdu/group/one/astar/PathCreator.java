83badea5f7673f10ca1201d8931afc88624c8ba9
OthelloEngineer <jolee18@student.sdu.dk>
2023-05-17 13:42:57 +0200

    added stats

--- AI/src/main/java/dk/sdu/group/one/astar/PathCreator.java
+++ AI/src/main/java/dk/sdu/group/one/astar/PathCreator.java
@@ -26,16 +26,19 @@ public class PathCreator implements AIservice {
     public List<Path> getPath(Coordinate currentCoordinate, Coordinate playerCoordinate) {
         List<Path> path = new ArrayList<>();
         try{
-            long startTime = System.currentTimeMillis()*1_000_000;
+            long startTime = System.nanoTime();
 
             ArrayList<Coordinate> coordinates = (ArrayList<Coordinate>) Node.aStar(currentCoordinate.clone(), playerCoordinate.clone(), Mappers.GenerateNodes(mapService));
 
-            long endTime = System.currentTimeMillis()*1_000_000;
+            long endTime = System.nanoTime();
             totalMeasurements += 1;
-            totalTime += (endTime - startTime);
-            loggingService.log(this.getClass(), "new measurement: " + (endTime - startTime) + " - deviance from average: " + (endTime - startTime)/(totalTime/totalMeasurements));
-            loggingService.log(this.getClass(), "Total logs: " + totalMeasurements + ", average runtime nanos: "
-                    + totalTime/totalMeasurements);
+            long time = (endTime - startTime);
+            totalTime += time;
+            double average = totalTime*1.0/totalMeasurements;
+            double deviationPercent = (1-(time*1.0/average))*100;
+            loggingService.log(this.getClass(), "new measurement: " + time + "ns - deviance from average: " + deviationPercent + "%");
+            loggingService.log(this.getClass(), "Total logs: " + totalMeasurements + ", average runtime: "
+                    + average + "ns");
 
             for (int i = 0; i < coordinates.size()-1; i++) {
                 path.add(new Path(coordinates.get(i), new Vector2(coordinates.get(i).getX()-coordinates.get(i+1).getX(),
